6cbe30aae485e569fb5d346d8e601a02e3e85ba9
colorrin <mb.andres@gmail.com>
2020-06-04 21:31:01 -0300

    Added Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :log do
+  	collection do
+  		post :entry
+  		get :entry
+  	end
+  end
 end
