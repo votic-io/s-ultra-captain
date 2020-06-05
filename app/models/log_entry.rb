fbec6d3c5387df3b01d5d5d257937a2847f2e455
colorrin <mb.andres@gmail.com>
2020-06-04 21:36:47 -0300

    Added Route

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -5,7 +5,7 @@ class LogEntry
 
 	field :message, type: String
 	field :binaries, type: Array, default: [] #[{name: String, remote_path: String}]
-	field :keyStore, type: Hash, default: {} #{key: Hash}
+	field :keystore, type: Hash, default: {} #{key: Hash}
 	field :tags, type: Array, default: [] #[Strings]
 
 	index({ bucket: 1, tags: 1 }, { background: true })
